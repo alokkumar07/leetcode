f0072f17addae6cdd393d9fde112775988e31a56
alok kumar <56335015+alokkumar07@users.noreply.github.com>
2022-03-07 03:51:49 +0530

    Time: 23 ms (62.68%), Space: 6.7 MB (99.07%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -20,23 +20,23 @@ public:
       int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m= text2.size();
-        //vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+       // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         vector<int> prev(m+1,0),cur(m+1,0);
         for(int j=0;j<=m;j++) prev[j]=0;
-        //for(int i=0;i<=n;i++) dp[i][0]=0;
+       // for(int i=0;i<=n;i++) dp[i][0]=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 
                 if(i==0 || j==0) return 0;
         
-       // if(dp[i][j]!=-1) return dp[i][j];
+        //if(dp[i][j]!=-1) return dp[i][j];
         
-        if(text1[i-1]== text2[j-1]) cur[j] =1 +prev [j-1];
+        if(text1[i-1]== text2[j-1]) cur[j] =1 +prev[j-1];
             
          else cur[j]= max(prev[j],cur[j-1]);
                 
             }
-            prev=cur;
+           prev=cur;
         }
         return prev[m];
         
